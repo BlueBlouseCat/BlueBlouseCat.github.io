e84a7fca70fbee60bbd4861e88aafffd5976dd4c
BlueBlouseCat <1922287870@qq.com>
2025-05-10 14:33:34 +0000

    5 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/22/first/","2025/04/22/hello-world/"];function toRandomPost(){
+var posts=["2025/05/10/hello-world/","2025/05/10/first/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

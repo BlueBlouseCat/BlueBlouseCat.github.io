f89920af42a6245a56140b4107ee6eb2945db4f2
BlueBlouseCat <1922287870@qq.com>
2025-05-11 07:10:40 +0000

    butterfly Updated By Github Actions

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2025/05/11/first/","2025/05/11/hello-world/","2025/05/11/tttt/","2025/05/11/xxx/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };

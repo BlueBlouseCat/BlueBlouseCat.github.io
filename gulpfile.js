f8fd9392387f0328f413e7f30085e244d48aebcd
BlueBlouseCat <1922287870@qq.com>
2025-12-28 19:36:59 +0800

    Site updated: 2025-12-28 19:36:59

--- gulpfile.js
+++ gulpfile.js
@@ -102,33 +102,33 @@ gulp.task("minify-html", () => {
 });
 
 //压缩字体
-function minifyFont(text, cb) {
-  gulp
-    .src("./public/fonts/*.ttf") //原字体所在目录
-    .pipe(
-      fontmin({
-        text: text,
-      })
-    )
-    .pipe(gulp.dest("./public/fontsdest/")) //压缩后的输出目录
-    .on("end", cb);
-}
+// function minifyFont(text, cb) {
+//   gulp
+//     .src("./public/fonts/*.ttf") //原字体所在目录
+//     .pipe(
+//       fontmin({
+//         text: text,
+//       })
+//     )
+//     .pipe(gulp.dest("./public/fontsdest/")) //压缩后的输出目录
+//     .on("end", cb);
+// }
 
-gulp.task("mini-font", cb => {
-  var buffers = [];
-  gulp
-    .src(["./public/**/*.html"]) //HTML文件所在目录请根据自身情况修改
-    .on("data", function (file) {
-      buffers.push(file.contents);
-    })
-    .on("end", function () {
-      var text = Buffer.concat(buffers).toString("utf-8");
-      minifyFont(text, cb);
-    });
-});
+// gulp.task("mini-font", cb => {
+//   var buffers = [];
+//   gulp
+//     .src(["./public/**/*.html"]) //HTML文件所在目录请根据自身情况修改
+//     .on("data", function (file) {
+//       buffers.push(file.contents);
+//     })
+//     .on("end", function () {
+//       var text = Buffer.concat(buffers).toString("utf-8");
+//       minifyFont(text, cb);
+//     });
+// });
 
 // 执行 gulp 命令时执行的任务
 gulp.task(
   "default",
-  gulp.series("generate-service-worker", gulp.parallel("compress", "minify-html", "minify-css", "mini-font"))
+  gulp.series("generate-service-worker", gulp.parallel("compress", "minify-html", "minify-css"))
 );
